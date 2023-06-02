111cff0e001472338f8d1ecf2b200c094c466f0c
lilianayumi <lilianayumi97@gmail.com>
2023-06-02 15:13:24 -0300

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AvatarComponent } from './components/avatar/avatar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { NewPostComponent } from './components/new-post/new-post.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AvatarComponent,
+    FooterComponent,
+    TweetComponent,
+    NewPostComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/new-post/new-post.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { MessagesService } from 'src/app/services/messages.service';
+
+@Component({
+  selector: 'app-new-post',
+  templateUrl: './new-post.component.html',
+  styleUrls: ['./new-post.component.css']
+})
+export class NewPostComponent {
+
+  tweetMaxLength: number = 240;
+  tweetRemain: number = this.tweetMaxLength;
+  tweetMessage: string = "";
+
+  constructor(private service: MessagesService) {}
+
+  changeTweet() {
+    this.tweetRemain = this.tweetMaxLength - this.tweetMessage.length;
+
+  }
+  sendTweet() {
+    this.service.addMessage(this.tweetMessage);
+    this.tweetMessage = "";
+    this.tweetRemain = this.tweetMaxLength;
+  }
+
+}
